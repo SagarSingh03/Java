da54566f86734ad2a3bbd2ca3c1e14d1c6dbb4fe
Sagar Singh <wasteStorage03@gmail.com>
2026-01-01 06:10:40 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables{
+    public static void main (String[] args){
+        int a = 10;
+        int b = 20; 
+        int c = 18;
+        int sum =  a + b + c;
+        System.out.println("Sum of all the Int values are: " + sum);
+        int simplification = a + b - c;
+        System.out.println("Val of Simplification : " + simplification);
+        
+    }
+}
